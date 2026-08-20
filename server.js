7f674d707cefd93dc4f916b56660c248f77bfd42
Hafsa Nazir <hafsa.binte.nazirr@gmail.com>
2026-08-20 20:04:20 +0500

    Prepare backend for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
-const PORT = 3000;
-app.listen(PORT,function(){
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,"0.0.0.0", function(){
 
 console.log(`Server is running on port ${PORT}`);
 
